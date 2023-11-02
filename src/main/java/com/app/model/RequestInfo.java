bead05c2245b592a6f370551569ea5ba87adea4f
Shankar tiwari <mastpoint@gmail.com>
2023-11-02 19:46:44 +0530

    shankar

--- src/main/java/com/app/model/RequestInfo.java
+++ src/main/java/com/app/model/RequestInfo.java
@@ -6,10 +6,13 @@ package com.app.model;
 
 import java.util.Map;
 
+import lombok.Data;
+
 /**
  *
  * @author z0043uwn
  */
+@Data
 public class RequestInfo {
 
     private String serverName;
@@ -28,129 +31,5 @@ public class RequestInfo {
     private Map<String, String> attributes;
     private Map<String, String[]> parameters;
 
-    public String getServerName() {
-        return serverName;
-    }
-
-    public void setServerName(String serverName) {
-        this.serverName = serverName;
-    }
-
-    public String getRemoteAddress() {
-        return remoteAddress;
-    }
-
-    public void setRemoteAddress(String remoteAddress) {
-        this.remoteAddress = remoteAddress;
-    }
-
-    public String getRemoteHost() {
-        return remoteHost;
-    }
-
-    public void setRemoteHost(String remoteHost) {
-        this.remoteHost = remoteHost;
-    }
-
-    public String getRemoteUser() {
-        return remoteUser;
-    }
-
-    public void setRemoteUser(String remoteUser) {
-        this.remoteUser = remoteUser;
-    }
-
-    public String getRequestURI() {
-        return requestURI;
-    }
-
-    public void setRequestURI(String requestURI) {
-        this.requestURI = requestURI;
-    }
-
-    public String getRequestedSessionId() {
-        return requestedSessionId;
-    }
-
-    public void setRequestedSessionId(String requestedSessionId) {
-        this.requestedSessionId = requestedSessionId;
-    }
-
-    public String getServletPath() {
-        return servletPath;
-    }
-
-    public void setServletPath(String servletPath) {
-        this.servletPath = servletPath;
-    }
-
-    public String getScheme() {
-        return scheme;
-    }
-
-    public void setScheme(String scheme) {
-        this.scheme = scheme;
-    }
-
-    public String getLocalName() {
-        return localName;
-    }
-
-    public void setLocalName(String localName) {
-        this.localName = localName;
-    }
-
-    public Integer getLocalPort() {
-        return localPort;
-    }
-
-    public void setLocalPort(Integer localPort) {
-        this.localPort = localPort;
-    }
-
-    public String getLocalCountry() {
-        return localCountry;
-    }
-
-    public void setLocalCountry(String localCountry) {
-        this.localCountry = localCountry;
-    }
-
-    public String getContextPath() {
-        return contextPath;
-    }
-
-    public void setContextPath(String contextPath) {
-        this.contextPath = contextPath;
-    }
-
-    public Map<String, String> getHeaders() {
-        return headers;
-    }
-
-    public void setHeaders(Map<String, String> headers) {
-        this.headers = headers;
-    }
-
-    public Map<String, String> getAttributes() {
-        return attributes;
-    }
-
-    public void setAttributes(Map<String, String> attributes) {
-        this.attributes = attributes;
-    }
-
-    public Map<String, String[]> getParameters() {
-        return parameters;
-    }
-
-    public void setParameters(Map<String, String[]> parameters) {
-        this.parameters = parameters;
-    }
-
-    @Override
-    public String toString() {
-        return "RequestInfo{" + "serverName=" + serverName + ", remoteAddress=" + remoteAddress + ", remoteHost=" + remoteHost + ", remoteUser=" + remoteUser + ", requestURI=" + requestURI + ", requestedSessionId=" + requestedSessionId + ", servletPath=" + servletPath + ", scheme=" + scheme + ", localName=" + localName + ", localPort=" + localPort + ", localCountry=" + localCountry + ", contextPath=" + contextPath + ", headers=" + headers + ", attributes=" + attributes + ", parameters=" + parameters + '}';
-    }
-
+  
 }
